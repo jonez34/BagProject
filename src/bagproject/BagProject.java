53b3699833467eef520d82855cb5ac70ba4e6465
hjones2368 <hjones2368@WS-AC-CTD-37006.suscorp.edu>
2016-01-25 19:53:55 -0500

    Added more to int Bag

--- src/bagproject/BagProject.java
+++ src/bagproject/BagProject.java
@@ -15,7 +15,44 @@ public class BagProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        TestIntegerBag();
+    }
+    public static void TestIntegerBag(){
+       
+        // TODO code application logic here
+        System.out.println("Hello");
+ //       int[] intarray = new int[10];
+ //      intarray[0] = 10;
+ //       intarray[1] = 1;
         
+        IntegerBag myBag = new IntegerBag();
+        
+        myBag.add(10);
+        myBag.add(11);
+        myBag.add(12);
+        myBag.add(13);
+        myBag.add(14);
+        myBag.add(15);
+        myBag.add(16);
+        myBag.add(17);
+        myBag.add(18);
+        myBag.add(19);
+        myBag.add(20);
+        myBag.add(21);
+        
+        myBag.remove();
+        myBag.remove();
+        myBag.remove();
+        System.out.println(myBag);
+        
+        myBag.removeItem(13);
+        //myBag.removeAt(3);
+        //myBag.clear();
+        //myBag.remove();
+        System.out.println(myBag);
+        myBag.removeItem(13);
+    
+    
     }
     
 }
